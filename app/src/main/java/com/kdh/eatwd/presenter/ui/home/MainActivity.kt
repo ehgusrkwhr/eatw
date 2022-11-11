fbf393fb43b254de35a22a21767aa3c7da7d438c
dodo <ehgus4688@gmail.com>
2022-11-11 18:01:09 +0900

    권한처리,location클래스 정의 ing..

--- app/src/main/java/com/kdh/eatwd/presenter/ui/home/MainActivity.kt
+++ app/src/main/java/com/kdh/eatwd/presenter/ui/home/MainActivity.kt
@@ -1,38 +1,128 @@
 package com.kdh.eatwd.presenter.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.kdh.eatwd.databinding.ActivityMainBinding
-import com.kdh.eatwd.presenter.ui.custom.CustomCircleProgressBar
+import com.kdh.eatwd.presenter.util.LocationUtil
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private val LOCATION_PERMISSON = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    private val PERMISSION_CODE = 100
+    private var requestPermissionLauncher: ActivityResultLauncher<Array<String>>? = null
+    lateinit var locationUtil: LocationUtil
+
+    // 위도와 경도를 저장
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn1.setOnClickListener {
-            viewModel.getAirInfo("40", "40")
-        }
+//        binding.btn1.setOnClickListener {
+//            viewModel.getAirInfo("40", "40")
+//        }
+        initView()
+        isPermissions()
         setObserver()
     }
 
-    fun setObserver(){
-        lifecycleScope.launch{
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.airInfo.collect{ pollution ->
+    private fun initView() {
+        locationUtil = LocationUtil(this@MainActivity)
+        locationUtil.g
+        viewModel.getAirInfo("40", "40")
+    }
+
+    private fun setObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.airInfo.collect { pollution ->
                     binding.tvPoll.text = pollution?.aqius.toString()
                 }
             }
         }
     }
+
+    private fun isPermissions() {
+
+        setPermissionLauncher()
+
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한요청!!
+            requestPermissionLauncher?.launch(LOCATION_PERMISSON)
+//            ActivityCompat.requestPermissions(this@MainActivity,LOCATION_PERMISSON,PERMISSION_CODE)
+        }
+    }
+
+    private fun setPermissionLauncher() {
+
+        requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { result: Map<String, Boolean> ->
+                val deniedList: List<String> = result.filter {
+                    !it.value
+                }.map {
+                    it.key
+                }
+                when {
+                    deniedList.isNotEmpty() -> {
+                        val map = deniedList.groupBy { permission ->
+                            if (shouldShowRequestPermissionRationale(permission)) "DENIED" else "EXPLAINED"
+                        }
+                        map["DENIED"]?.let {
+                            Log.d("dodo55 ", "한번거부");
+                            ActivityCompat.requestPermissions(
+                                this@MainActivity,
+                                LOCATION_PERMISSON,
+                                PERMISSION_CODE
+                            )
+                        }
+                        map["EXPLAINED"]?.let {
+                            Log.d("dodo55 ", "두번거부");
+                            ActivityCompat.requestPermissions(
+                                this@MainActivity,
+                                LOCATION_PERMISSON,
+                                PERMISSION_CODE
+                            )
+                        }
+                    }
+                    else -> {
+                        Log.d("dodo55 ", "통과");
+                    }
+                }
+            }
+
+    }
+
+
 }
--- app/src/main/java/com/kdh/eatwd/presenter/util/LocationUtil.kt
+++ app/src/main/java/com/kdh/eatwd/presenter/util/LocationUtil.kt
@@ -1,4 +1,81 @@
 package com.kdh.eatwd.presenter.util
 
-class LocationUtil {
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+
+class LocationUtil(private val context: Context) {
+
+    //Location Manager는 시스템 위치 서비스에 접근을 제공하는 클래스.
+    private val locationManager: LocationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    //Location 클래스는 위도, 경도, 고도와 같이 위치에 관련된 정보를 가지고 있는 데이터 클래스.
+    private var location: Location? = null
+
+    private fun fetchLocationInfo(): Location? {
+        try {
+            var gpsLocation: Location? = null
+            var networkLocation: Location? = null
+
+            val isGPSEnabled: Boolean =
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled: Boolean =
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                return null
+            } else {
+                val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION // ACCESS_COARSE_LOCATION 보다 더 정밀한 위치 정보를 얻을 수 있습니다.
+                )
+                val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION // 도시 Block 단위의 정밀도의 위치 정보를 얻을 수 있습니다.
+                )
+                //만약 위 두 개 권한 없다면 null을 반환합니다.
+                if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
+                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED
+                ) return null
+
+                if (isGPSEnabled) {
+                    gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                }
+                if (isNetworkEnabled) {
+                    networkLocation =
+                        locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                }
+
+                if (gpsLocation != null && networkLocation != null) {
+                    return if (gpsLocation.accuracy > networkLocation.accuracy) {
+                        gpsLocation
+                    } else {
+                        networkLocation
+                    }
+                }else{
+                    if(gpsLocation != null) return gpsLocation
+                    if(networkLocation != null) return networkLocation
+                }
+
+            }
+
+
+        } catch (e: Exception) {
+
+        }
+        return null
+    }
+
+    fun getLocationLatitude() : Double{
+        return location?.latitude ?: 0.0
+    }
+
+    fun getLocationLongitude() : Double{
+        return location?.longitude ?: 0.0
+    }
+
 }
